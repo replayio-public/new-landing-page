d02edb0e63ebacd18cbf005d6f343ca7f25af250
Jason Laster <jason.laster.11@gmail.com>
2024-03-30 11:43:14 -0700

    Add pricing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -33,5 +33,5 @@ export default {
       },
     },
   },
-  plugins: [formsPlugin, headlessuiPlugin],
+  plugins: [formsPlugin, headlessuiPlugin, require("tailwindcss-animate")],
 } satisfies Config
