521d63111c2ca0c35dd008e971870acefc6805c0
Jon Bell <github@lot23.com>
2024-04-03 20:27:17 +1300

    Merge cleanup

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -16,11 +16,8 @@ const variantStyles = {
     blue: `${styles.bgStandardGreen} text-white hover:text-slate-100 hover:bg-blue-500 active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600`,
     white:
       'bg-white text-slate-900 hover:bg-gray-50 active:bg-gray-200 active:text-slate-600 focus-visible:outline-white',
-<<<<<<< HEAD
-    gray: 'bg-white text-slate-900 hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white',
-=======
-    gray: 'bg-white text-slate-900 hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white'
->>>>>>> 7531e684414a857c34510a09870d7997270bee09
+    gray:
+      'bg-white text-slate-900 hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white',
   },
   outline: {
     slate:
@@ -28,30 +25,26 @@ const variantStyles = {
     blue: 'ring-slate-200 text-slate-700 hover:text-slate-900 hover:ring-slate-300 active:bg-slate-100 active:text-slate-600 focus-visible:outline-blue-600 focus-visible:ring-slate-300',
     white:
       'ring-slate-700 text-white hover:ring-slate-500 active:ring-slate-700 active:text-slate-400 focus-visible:outline-white',
-<<<<<<< HEAD
-    gray: 'ring-slate-700 text-white hover:ring-slate-500 active:ring-slate-700 active:text-slate-400 focus-visible:outline-white',
+    gray:
+      'ring-slate-700 text-white hover:ring-slate-500 active:ring-slate-700 active:text-slate-400 focus-visible:outline-white',
   },
-=======
-    gray: 'ring-slate-700 text-white hover:ring-slate-500 active:ring-slate-700 active:text-slate-400 focus-visible:outline-white'
-  }
->>>>>>> 7531e684414a857c34510a09870d7997270bee09
 }
 
 type ButtonProps = (
   | {
-      variant?: 'solid'
-      color?: keyof typeof variantStyles.solid
-    }
+    variant?: 'solid'
+    color?: keyof typeof variantStyles.solid
+  }
   | {
-      variant: 'outline'
-      color?: keyof typeof variantStyles.outline
-    }
+    variant: 'outline'
+    color?: keyof typeof variantStyles.outline
+  }
 ) &
   (
     | Omit<React.ComponentPropsWithoutRef<typeof Link>, 'color'>
     | (Omit<React.ComponentPropsWithoutRef<'button'>, 'color'> & {
-        href?: undefined
-      })
+      href?: undefined
+    })
   )
 export function Button({ className, ...props }: ButtonProps) {
   const variant = props.variant || ('solid' as keyof typeof variantStyles)
@@ -87,13 +80,7 @@ export function BaseHubButton({
   className = clsx(
     baseStyles[props.variant as Variant],
     props.variant === 'outline'
-<<<<<<< HEAD
-      ? variantStyles.outline[
-          props.color as keyof (typeof variantStyles)['outline']
-        ]
-=======
-      ? variantStyles.outline[props.color as keyof (typeof variantStyles)['outline']]
->>>>>>> 7531e684414a857c34510a09870d7997270bee09
+      ? variantStyles.outline[props.color as keyof typeof variantStyles['outline']]
       : props.variant === 'solid'
         ? variantStyles.solid[props.color]
         : undefined,
