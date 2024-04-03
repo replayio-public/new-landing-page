b5272a81d430af43010de80f2e3988db83a0fe22
Jon Bell <github@lot23.com>
2024-04-03 20:31:24 +1300

    Merge stuff

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ const baseStyles = {
   solid:
     'group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2',
   outline:
-    'group inline-flex ring-1 items-center justify-center rounded-full py-2 px-4 text-sm focus:outline-none'
+    'group inline-flex ring-1 items-center justify-center rounded-full py-2 px-4 text-sm focus:outline-none',
 }
 
 const variantStyles = {
@@ -16,8 +16,7 @@ const variantStyles = {
     blue: `${styles.bgStandardGreen} text-white hover:text-slate-100 hover:bg-blue-500 active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600`,
     white:
       'bg-white text-slate-900 hover:bg-gray-50 active:bg-gray-200 active:text-slate-600 focus-visible:outline-white',
-    gray:
-      'bg-white text-slate-900 hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white',
+    gray: 'bg-white text-slate-900 hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white',
   },
   outline: {
     slate:
@@ -25,8 +24,7 @@ const variantStyles = {
     blue: 'ring-slate-200 text-slate-700 hover:text-slate-900 hover:ring-slate-300 active:bg-slate-100 active:text-slate-600 focus-visible:outline-blue-600 focus-visible:ring-slate-300',
     white:
       'ring-slate-700 text-white hover:ring-slate-500 active:ring-slate-700 active:text-slate-400 focus-visible:outline-white',
-    gray:
-      'ring-slate-700 text-white hover:ring-slate-500 active:ring-slate-700 active:text-slate-400 focus-visible:outline-white',
+    gray: 'ring-slate-700 text-white hover:ring-slate-500 active:ring-slate-700 active:text-slate-400 focus-visible:outline-white',
   },
 }
 
@@ -50,7 +48,11 @@ export function Button({ className, ...props }: ButtonProps) {
   const variant = props.variant || ('solid' as keyof typeof variantStyles)
   const color = props.color || ('slate' as keyof typeof variantStyles.solid)
 
-  className = clsx(baseStyles[variant], variantStyles[variant][color], className)
+  className = clsx(
+    baseStyles[variant],
+    variantStyles[variant][color],
+    className,
+  )
 
   return typeof props.href === 'undefined' ? (
     <button className={className} {...props} {...props} />
@@ -80,11 +82,13 @@ export function BaseHubButton({
   className = clsx(
     baseStyles[props.variant as Variant],
     props.variant === 'outline'
-      ? variantStyles.outline[props.color as keyof typeof variantStyles['outline']]
+      ? variantStyles.outline[
+      props.color as keyof (typeof variantStyles)['outline']
+      ]
       : props.variant === 'solid'
         ? variantStyles.solid[props.color]
         : undefined,
-    className
+    className,
   )
 
   return typeof props.href === 'undefined' ? (
