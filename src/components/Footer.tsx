b447a40eef22c77b840df7dd367bcfe597692b1b
Aditya Pandey <aditya@autospace.co>
2024-04-04 11:09:41 -0600

    Fix lint errors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ type LinkItem = {
   icon?: (props: any) => JSX.Element
 }
 
-const showNewsletter = false;
+const showNewsletter = false
 
 function Category({ name, links }: { name: string; links: LinkItem[] }) {
   return (
@@ -171,38 +171,40 @@ export function Footer() {
             </div>
           </div>
         </div>
-        {showNewsletter && <div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 lg:mt-24 lg:flex lg:items-center lg:justify-between">
-          <div>
-            <h3 className="text-sm font-semibold leading-6 text-gray-900">
-              Subscribe to our newsletter
-            </h3>
-            <p className="mt-2 text-sm leading-6 text-gray-600">
-              The latest news, articles, and resources, sent to your inbox weekly.
-            </p>
-          </div>
-          <form className="mt-6 sm:flex sm:max-w-md lg:mt-0">
-            <label htmlFor="email-address" className="sr-only">
-              Email address
-            </label>
-            <input
-              type="email"
-              name="email-address"
-              id="email-address"
-              autoComplete="email"
-              required
-              className="w-full min-w-0 appearance-none rounded-md border-0 bg-white px-3 py-1.5 text-base text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:w-56 sm:text-sm sm:leading-6"
-              placeholder="Enter your email"
-            />
-            <div className="mt-4 sm:ml-4 sm:mt-0 sm:flex-shrink-0">
-              <button
-                type="submit"
-                className={`flex w-full items-center justify-center rounded-md ${styles.bgPrimaryAccent} px-3 py-2 text-sm font-semibold text-white shadow-sm hover:${styles.bgPrimaryAccent} focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-${styles.bgPrimaryAccent}`}
-              >
-                Subscribe
-              </button>
+        {showNewsletter && (
+          <div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 lg:mt-24 lg:flex lg:items-center lg:justify-between">
+            <div>
+              <h3 className="text-sm font-semibold leading-6 text-gray-900">
+                Subscribe to our newsletter
+              </h3>
+              <p className="mt-2 text-sm leading-6 text-gray-600">
+                The latest news, articles, and resources, sent to your inbox weekly.
+              </p>
             </div>
-          </form>
-        </div>}
+            <form className="mt-6 sm:flex sm:max-w-md lg:mt-0">
+              <label htmlFor="email-address" className="sr-only">
+                Email address
+              </label>
+              <input
+                type="email"
+                name="email-address"
+                id="email-address"
+                autoComplete="email"
+                required
+                className="w-full min-w-0 appearance-none rounded-md border-0 bg-white px-3 py-1.5 text-base text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:w-56 sm:text-sm sm:leading-6"
+                placeholder="Enter your email"
+              />
+              <div className="mt-4 sm:ml-4 sm:mt-0 sm:flex-shrink-0">
+                <button
+                  type="submit"
+                  className={`flex w-full items-center justify-center rounded-md ${styles.bgPrimaryAccent} px-3 py-2 text-sm font-semibold text-white shadow-sm hover:${styles.bgPrimaryAccent} focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-${styles.bgPrimaryAccent}`}
+                >
+                  Subscribe
+                </button>
+              </div>
+            </form>
+          </div>
+        )}
         <div className="mt-8 border-t border-gray-900/10 pt-8 md:flex md:items-center md:justify-between">
           <div className="flex space-x-6 md:order-2">
             {navigation.social.map((item) => (
