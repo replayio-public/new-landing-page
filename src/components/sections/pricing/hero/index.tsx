61426d1d372819d65460c754e225aca8dd06c2ac
Aditya Pandey <aditya@autospace.co>
2024-04-05 17:33:21 -0600

    Switch to typeform links

--- src/components/sections/pricing/hero/index.tsx
+++ src/components/sections/pricing/hero/index.tsx
@@ -22,7 +22,7 @@ const bugreportingPlans = [
     price: 20,
     mode: 'per month / per developer',
     cta: 'Create team',
-    link: 'https://app.replay.io/team/new',
+    link: 'https://replayio.typeform.com/to/jTudlerL',
     features: [
       '200 recordings per month',
       'Up to 10 users + developers',
@@ -35,7 +35,7 @@ const bugreportingPlans = [
     price: 75,
     mode: 'per month / per developer',
     cta: 'Create Organization',
-    link: 'https://app.replay.io/org/new',
+    link: 'https://replayio.typeform.com/to/jTudlerL',
     features: [
       '1,000 recordings per month',
       'Up to 100 users + developers',
